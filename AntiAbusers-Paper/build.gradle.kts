c5ac1dba96f1430cd56ab8cf9f448c181f387bdf
Angelillo15 <contact@angelillo15.es>
2023-09-05 13:12:55 +0200

    process resources to change version

--- AntiAbusers-Paper/build.gradle.kts
+++ AntiAbusers-Paper/build.gradle.kts
@@ -3,3 +3,9 @@ dependencies {
     compileOnly(libs.worldguard)
     compileOnly(project(":AntiAbusers-API"))
 }
+
+tasks.processResources {
+    filesMatching("paper-plugin.yml") {
+        expand("version" to (parent?.version ?: project.version))
+    }
+}
