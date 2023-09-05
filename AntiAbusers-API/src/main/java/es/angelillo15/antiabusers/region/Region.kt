3523fc03d18827a88fedb6fb368d1e338c4ba0b8
Angelillo15 <contact@angelillo15.es>
2023-09-05 21:39:27 +0200

    Fix serializer

--- AntiAbusers-API/src/main/java/es/angelillo15/antiabusers/region/Region.kt
+++ AntiAbusers-API/src/main/java/es/angelillo15/antiabusers/region/Region.kt
@@ -3,16 +3,19 @@ package es.angelillo15.antiabusers.region
 import com.google.inject.Inject
 import es.angelillo15.antiabusers.AntiAbusersInstance
 import es.angelillo15.antiabusers.thread.executeAsync
+import org.bukkit.inventory.ItemStack
+import org.bukkit.util.io.BukkitObjectInputStream
+import org.bukkit.util.io.BukkitObjectOutputStream
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.InputStream
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 
-class Region @Inject constructor(val regionData: RegionData, val antiAbusersInstance: AntiAbusersInstance) {
-  private val folder = antiAbusersInstance.getPluginDataFolder().resolve("regions/")
+class Region @Inject constructor(val regionData: RegionData, private val antiAbusersInstance: AntiAbusersInstance) {
+  private val folder = antiAbusersInstance.getPluginDataFolder().resolve("regions")
   private val finalFile = FileOutputStream(folder.resolve("${regionData.id}.anab"))
-  private val writer = ObjectOutputStream(finalFile)
+  private val writer = BukkitObjectOutputStream(finalFile)
 
   init {
     if (!folder.exists()) {
@@ -21,10 +24,8 @@ class Region @Inject constructor(val regionData: RegionData, val antiAbusersInst
   }
 
   fun write() {
-    executeAsync {
-      antiAbusersInstance.getPluginLogger().debug("Writing binary for region ${regionData.id}")
-      writer.writeObject(regionData)
-    }
+    antiAbusersInstance.getPluginLogger().debug("Writing binary for region ${regionData.id}")
+    writer.writeObject(regionData)
   }
 
   companion object {
@@ -36,12 +37,12 @@ class Region @Inject constructor(val regionData: RegionData, val antiAbusersInst
         )
       } catch (e: Exception) {
         instance.getPluginLogger().debug("Region $regionID has no binary file")
-        val region = Region(RegionData(regionID, emptyList()), instance)
+        val region = Region(RegionData(regionID, ArrayList()), instance)
         region.write()
         return region
       }
 
-      val reader = ObjectInputStream(fileInputStream)
+      val reader = BukkitObjectInputStream(fileInputStream)
       val regionData = reader.readObject() as RegionData
 
       return Region(regionData, instance)
--- AntiAbusers-API/src/main/java/es/angelillo15/antiabusers/region/RegionData.kt
+++ AntiAbusers-API/src/main/java/es/angelillo15/antiabusers/region/RegionData.kt
@@ -3,4 +3,4 @@ package es.angelillo15.antiabusers.region
 import org.bukkit.inventory.ItemStack
 import java.io.Serializable
 
-class RegionData(val id: String, val blockedItems: List<ItemStack>) : Serializable
+class RegionData(val id: String, val blockedItems: ArrayList<ItemStack>) : Serializable
