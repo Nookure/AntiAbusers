d16ef538ad00d774055503441fee72bb2f5af94d
Angelillo15 <contact@angelillo15.es>
2023-09-05 12:42:33 +0200

    Added fields to the AntiAbusersInstance interface

--- AntiAbusers-API/src/main/java/es/angelillo15/antiabusers/AntiAbusersInstance.kt
+++ AntiAbusers-API/src/main/java/es/angelillo15/antiabusers/AntiAbusersInstance.kt
@@ -1,6 +1,19 @@
 package es.angelillo15.antiabusers
 
+import es.angelillo15.core.Logger
 import es.angelillo15.core.NookInstance
+import org.bukkit.entity.Player
 
 interface AntiAbusersInstance : NookInstance {
+  fun createPlayer(player: Player?): AntiAbuserPlayer?
+
+  fun loadHandlers()
+
+  fun loadListeners()
+
+  fun reload()
+
+  fun getPluginLogger() : Logger
+
+  fun loadConfig()
 }
