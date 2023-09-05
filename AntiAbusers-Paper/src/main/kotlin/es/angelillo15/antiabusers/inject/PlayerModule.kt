f017fbd2cd85e86f532f0e6d7ac762dd6c306bc4
Angelillo15 <contact@angelillo15.es>
2023-09-05 19:23:43 +0200

    edit player module

--- AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/inject/PlayerModule.kt
+++ AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/inject/PlayerModule.kt
@@ -3,7 +3,7 @@ package es.angelillo15.antiabusers.inject
 import es.angelillo15.antiabusers.PaperAntiAbuserPlayer
 import org.bukkit.entity.Player
 
-class PlayerModule(val player: Player) : PluginModule() {
+class PlayerModule(private val player: Player) : PluginModule() {
   override fun configure() {
     super.configure()
     bind(Player::class.java).toInstance(player)
