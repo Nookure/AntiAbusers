a924be877b687f0327800c7ad4df5670498742f7
Angelillo15 <contact@angelillo15.es>
2023-09-05 21:40:08 +0200

    Perks to SelectRegion to use paginated gui

--- AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/gui/SelectRegion.kt
+++ AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/gui/SelectRegion.kt
@@ -3,34 +3,34 @@ package es.angelillo15.antiabusers.gui
 import com.google.inject.Inject
 import com.sk89q.worldedit.bukkit.BukkitAdapter
 import com.sk89q.worldguard.WorldGuard
-import es.angelillo15.antiabusers.config.tl
+import es.angelillo15.antiabusers.utils.raw
+import es.angelillo15.antiabusers.utils.tl
 import es.angelillo15.core.Logger
-import es.angelillo15.core.libs.obliviate.inventory.Gui
 import es.angelillo15.core.libs.obliviate.inventory.Icon
-import es.angelillo15.core.libs.obliviate.inventory.pagination.PaginationManager
+import es.angelillo15.core.utils.TextUtils
 import net.kyori.adventure.text.Component
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryOpenEvent
 import org.bukkit.inventory.ItemStack
 
-class SelectRegion @Inject constructor(val player: Player) : Gui(player,"select-region-gui","pitos", 6) {
+class SelectRegion @Inject constructor(val player: Player) : PaginatedGui(
+  player,
+  "select-region-gui",
+  TextUtils.simpleColorize(raw("Gui.selectRegionTitle")),
+  6
+) {
   @Inject
   private lateinit var logger: Logger
 
-  private val paginationManager = PaginationManager(this)
   override fun onOpen(event: InventoryOpenEvent) {
-    paginationManager.registerPageSlotsBetween(10, 16);
-    paginationManager.registerPageSlotsBetween(19, 25);
-    paginationManager.registerPageSlotsBetween(28, 34);
-    paginationManager.registerPageSlotsBetween(37, 43);
-
     addRegions(player)
-
-    paginationManager.update()
+    load()
   }
 
   fun addRegions(player: Player) {
+    fillGui(ItemStack(Material.GRAY_STAINED_GLASS_PANE))
+
     val regionManager = WorldGuard.getInstance().platform.regionContainer.get(BukkitAdapter.adapt(player.world))
 
     if (regionManager == null || regionManager.regions.isEmpty()) {
@@ -39,7 +39,7 @@ class SelectRegion @Inject constructor(val player: Player) : Gui(player,"select-
     }
 
     val items = Material.entries.toTypedArray()
-    var i = 1
+    var i = 10
 
     regionManager.regions.forEach { region ->
       val itemStack = ItemStack(items[i++])
@@ -47,19 +47,11 @@ class SelectRegion @Inject constructor(val player: Player) : Gui(player,"select-
       itemMeta.displayName(Component.text(region.value.id).asComponent())
       itemStack.itemMeta = itemMeta
 
-      paginationManager.addItem(Icon(itemStack))
+      paginationManager.addItem(Icon(itemStack).onClick {
+        RegionSettings(player, region.value).open()
+      })
 
       logger.debug("Added region ${region.value.id} to the gui with material ${itemStack.type.name}")
     }
-
-    items.forEach {
-      val itemStack = ItemStack(it)
-
-      paginationManager.addItem(Icon(itemStack))
-    }
-  }
-
-  fun addNavigationButtons() {
-
   }
 }
