f547037ac3e48b8fc77afef1fe14b24a980dfa08
Angelillo15 <contact@angelillo15.es>
2023-09-05 15:11:55 +0200

    Added new classes to PluginModule

--- AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/inject/PluginModule.kt
+++ AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/inject/PluginModule.kt
@@ -6,9 +6,13 @@ import com.sk89q.worldguard.protection.regions.RegionContainer
 import es.angelillo15.antiabusers.AntiAbusers
 import es.angelillo15.antiabusers.AntiAbusersInstance
 import es.angelillo15.antiabusers.config.ConfigManager
+import es.angelillo15.antiabusers.config.Messages
 import es.angelillo15.antiabusers.utils.PluginLogger
 import es.angelillo15.core.Logger
 import es.angelillo15.core.NookInstance
+import es.angelillo15.core.messages.CoreMessages
+import net.kyori.adventure.platform.AudienceProvider
+import net.kyori.adventure.platform.bukkit.BukkitAudiences
 
 open class PluginModule : AbstractModule() {
   override fun configure() {
@@ -19,5 +23,7 @@ open class PluginModule : AbstractModule() {
     bind(AntiAbusers::class.java).toInstance(AntiAbusers.instance)
     bind(ConfigManager::class.java).asEagerSingleton()
     bind(NookInstance::class.java).toInstance(AntiAbusers.instance)
+    bind(CoreMessages::class.java).to(Messages::class.java).asEagerSingleton()
+    bind(AudienceProvider::class.java).toInstance(BukkitAudiences.create(AntiAbusers.instance))
   }
 }
