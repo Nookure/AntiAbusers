d0f32f9261db5f2e760f165712ab2a6d9d2c908a
Angelillo15 <contact@angelillo15.es>
2023-09-05 13:12:42 +0200

    Change to logger

--- AntiAbusers-API/src/main/java/es/angelillo15/antiabusers/config/ConfigManager.kt
+++ AntiAbusers-API/src/main/java/es/angelillo15/antiabusers/config/ConfigManager.kt
@@ -1,24 +1,30 @@
 package es.angelillo15.antiabusers.config
 
+import com.google.inject.Inject
 import com.google.inject.Singleton
 import es.angelillo15.antiabusers.AntiAbusersInstance
+import es.angelillo15.core.Logger
 import es.angelillo15.core.config.Config
 
 @Singleton
-class ConfigManager : Config() {
+@Suppress("ConvertSecondaryConstructorToPrimary")
+class ConfigManager : Config {
   lateinit var config: es.angelillo15.core.libs.config.manager.ConfigManager
     private set
   lateinit var messages: es.angelillo15.core.libs.config.manager.ConfigManager
     private set
-  private var instance = plugin as AntiAbusersInstance
+  private val logger: Logger
 
   companion object {
     lateinit var configManager: ConfigManager
       private set
   }
 
-  init {
+  @Inject
+  constructor(logger: Logger, instance: AntiAbusersInstance) {
     configManager = this
+    this.logger = logger
+    this.plugin = instance
   }
 
   fun load() {
@@ -29,18 +35,18 @@ class ConfigManager : Config() {
   private fun loadConfig() {
     config = loadFile("config.yml", "config.yml")
     plugin.setDebug(config.config.getBoolean("Config.debug"))
-    instance.getPluginLogger().debug("Config file loaded!")
+    logger.debug("Config file loaded!")
   }
 
   private fun loadMessages() {
-    instance.getPluginLogger().debug("Creating messages file...")
+    logger.debug("Creating messages file...")
     loadFile("lang/en.yml", "lang/en.yml")
     loadFile("lang/es.yml", "lang/es.yml")
 
     val lang = config.config.getString("Config.language")
-    instance.getPluginLogger().debug("Loading $lang.yml file...")
+    logger.debug("Loading $lang.yml file...")
 
     messages = loadFile("lang/$lang.yml", "lang/$lang.yml")
-    instance.getPluginLogger().debug("Messages file loaded!")
+    logger.debug("Messages file loaded!")
   }
 }
