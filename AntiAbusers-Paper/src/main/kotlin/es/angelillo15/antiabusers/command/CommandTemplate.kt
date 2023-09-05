d09c2fa30b66439cdd0ad9ef37f7cc88ef5a33a8
Angelillo15 <contact@angelillo15.es>
2023-09-05 15:11:38 +0200

    create command template

---
+++ AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/command/CommandTemplate.kt
@@ -0,0 +1,38 @@
+package es.angelillo15.antiabusers.command
+
+import es.angelillo15.core.cmd.Command
+import es.angelillo15.core.cmd.sender.BukkitConsoleCommandSender
+import es.angelillo15.core.cmd.sender.PlayerCommandSender
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class CommandTemplate : org.bukkit.command.Command {
+  private val command: Command
+
+  constructor(name: String, command: Command) : super(name) {
+    this.command = command
+  }
+
+  constructor(name: String, command: Command, permission: String) : super(name) {
+    this.command = command
+    setPermission(permission)
+  }
+
+  constructor(name: String, command: Command, permission: String, vararg aliases: String) : super(name) {
+    this.command = command
+    setPermission(permission)
+    setAliases(aliases.toList())
+  }
+
+  override fun execute(sender: CommandSender, commandLabel: String, args: Array<String>): Boolean {
+
+    val commandSender = if (sender is Player) {
+      PlayerCommandSender(sender)
+    } else {
+      BukkitConsoleCommandSender()
+    }
+
+    command.onCommand(commandSender, commandLabel, args);
+    return true
+  }
+}
