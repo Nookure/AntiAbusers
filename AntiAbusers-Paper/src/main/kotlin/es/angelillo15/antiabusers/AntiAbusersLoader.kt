8162f4dd3bde33619560433d4aaffe5b5151fd55
Angelillo15 <contact@angelillo15.es>
2023-09-05 13:06:31 +0200

    Added methods to load

--- AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/AntiAbusersLoader.kt
+++ AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/AntiAbusersLoader.kt
@@ -2,6 +2,15 @@ package es.angelillo15.antiabusers
 
 class AntiAbusersLoader : AntiAbusers() {
   override fun onEnable() {
+    super.onEnable()
     pPluginLogger.info("Loading AntiAbusers...")
+    loadConfig()
+    loadHandlers()
+    loadListeners()
+  }
+
+  override fun onDisable() {
+    pPluginLogger.info("Disabling AntiAbusers...")
+    unregisterListener()
   }
 }
