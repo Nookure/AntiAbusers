17c0299116b4ca6ddc1ee7bbae1a2b8ee7af18f7
Angelillo15 <contact@angelillo15.es>
2023-09-06 15:28:22 +0200

    On new item scheduler task

--- AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/listener/OnInventoryNewItem.kt
+++ AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/listener/OnInventoryNewItem.kt
@@ -1,18 +1,30 @@
 package es.angelillo15.antiabusers.listener
 
 import com.google.inject.Inject
+import es.angelillo15.antiabusers.AntiAbusers
+import es.angelillo15.antiabusers.manager.AntiAbusersPlayers
+import es.angelillo15.antiabusers.manager.RegionManager
+import es.angelillo15.antiabusers.thread.executeAsync
 import es.angelillo15.core.Logger
+import org.bukkit.Bukkit
 import org.bukkit.GameMode
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.ClickType
 import org.bukkit.event.inventory.InventoryAction
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryType
+import org.bukkit.scheduler.BukkitRunnable
+import java.util.function.Consumer
 
 class OnInventoryNewItem : Listener {
   @Inject
   private lateinit var logger: Logger
+  @Inject
+  private lateinit var antiAbusersPlayers: AntiAbusersPlayers
+  @Inject
+  private lateinit var plugin: AntiAbusers
 
   @EventHandler
   fun onInventoryNewItem(event: InventoryClickEvent) {
@@ -34,5 +46,11 @@ class OnInventoryNewItem : Listener {
     ) {
       return
     }
+    val player = event.whoClicked as Player
+    val abuserPlayer = antiAbusersPlayers.getPlayer(player.name) ?: return
+
+    Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, Consumer {
+      abuserPlayer.check(true)
+    }, 1)
   }
 }
