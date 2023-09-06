84f1010c6f656ee9aaac3ef534b38c83b3fdd0be
Angelillo15 <contact@angelillo15.es>
2023-09-06 15:25:21 +0200

    Added inv check

--- AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/PaperAntiAbuserPlayer.kt
+++ AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/PaperAntiAbuserPlayer.kt
@@ -4,31 +4,29 @@ import com.google.inject.Inject
 import com.sk89q.worldedit.bukkit.BukkitAdapter
 import com.sk89q.worldguard.protection.regions.ProtectedRegion
 import com.sk89q.worldguard.protection.regions.RegionContainer
+import es.angelillo15.antiabusers.factory.PlayerFactory
+import es.angelillo15.antiabusers.manager.RegionManager
+import es.angelillo15.core.Logger
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import java.util.concurrent.ConcurrentHashMap
 
 class PaperAntiAbuserPlayer @Inject constructor(
-  private val player: Player,
-  private val regionContainer: RegionContainer
-) : AntiAbuserPlayer {
-
+  private val regionContainer: RegionContainer,
+  private val regionManager: RegionManager,
+  private val logger: Logger
+) : AntiAbuserPlayer, PlayerFactory<PaperAntiAbuserPlayer> {
+  private var player: Player? = null
   private var abuser = false
   private var checking = false
   private var attack = false
-  private var illegalItems = ArrayList<ItemStack>()
+  private var illegalItems = ConcurrentHashMap<String, ArrayList<ItemStack>>()
   private var currentRegions = ArrayList<String>()
-
-  init {
-    reloadRegionList()
-  }
+  private var abuserRegions = ConcurrentHashMap<String, Boolean>()
 
   override fun getName(): String {
-    return player.name
-  }
-
-  override fun isAbuser(): Boolean {
-    return abuser
+    return player!!.name
   }
 
   override fun setAbuser(abuser: Boolean) {
@@ -43,26 +41,46 @@ class PaperAntiAbuserPlayer @Inject constructor(
     return checking
   }
 
-  override fun check() {
+  override fun check(force: Boolean) {
+    val start = System.currentTimeMillis()
 
+    checking = force
+    illegalItems.clear()
+    abuserRegions.clear()
+
+    regionManager.getRegions().values.forEach {
+      logger.debug("Checking region ${it.regionData.id}")
+      it.regionData.blockedItems.forEach { item ->
+        if (player!!.inventory.containsAtLeast(item, 1)) {
+          illegalItems.computeIfAbsent(it.regionData.id) { ArrayList() }.add(item)
+          abuserRegions[it.regionData.id] = true
+          logger.debug("Player ${player!!.name} has illegal item ${item.type} in region ${it.regionData.id}")
+        }
+      }
+    }
+
+    checking = false
+
+    val end = System.currentTimeMillis()
+    logger.debug("Player ${player!!.name} checked in ${end - start}ms")
   }
 
   override fun canAttack(player: AntiAbuserPlayer): Boolean {
     return attack
   }
 
-  override fun getIllegalItems(): ArrayList<ItemStack> {
+  override fun getIllegalItems(): Map<String, ArrayList<ItemStack>> {
     return illegalItems
   }
 
   override fun sendMessage(component: Component) {
-    player.sendMessage(component)
+    player!!.sendMessage(component)
   }
 
   fun reloadRegionList() {
     currentRegions.clear()
 
-    regionContainer.createQuery().getApplicableRegions(BukkitAdapter.adapt(player.location)).forEach { region ->
+    regionContainer.createQuery().getApplicableRegions(BukkitAdapter.adapt(player!!.location)).forEach { region ->
       currentRegions.add(region.id)
     }
   }
@@ -75,5 +93,15 @@ class PaperAntiAbuserPlayer @Inject constructor(
     entered.forEach { region ->
       currentRegions.add(region.id)
     }
+  }
+
+  override fun isAbuser(regionID: String): Boolean {
+    return abuserRegions[regionID] ?: false
+  }
+
+  override fun create(player: Player): PaperAntiAbuserPlayer {
+    this.player = player
+    reloadRegionList()
+    return this
   }
 }
