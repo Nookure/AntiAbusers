68469875034ca028c9ef39cae164d98032b77c04
Angelillo15 <contact@angelillo15.es>
2023-09-06 13:19:31 +0200

    Added edit items gui

--- AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/gui/EditItems.kt
+++ AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/gui/EditItems.kt
@@ -1,20 +1,48 @@
 package es.angelillo15.antiabusers.gui
 
 import com.sk89q.worldguard.protection.regions.ProtectedRegion
+import es.angelillo15.antiabusers.AntiAbusers
+import es.angelillo15.antiabusers.region.Region
+import es.angelillo15.antiabusers.utils.cmp
 import es.angelillo15.antiabusers.utils.raw
+import es.angelillo15.core.libs.obliviate.inventory.Icon
 import es.angelillo15.core.libs.obliviate.inventory.pagination.PaginationManager
 import es.angelillo15.core.utils.TextUtils
+import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryOpenEvent
+import org.bukkit.inventory.ItemStack
 
 class EditItems(val player: Player, val region: ProtectedRegion) : PaginatedGui(
   player,
-  "add-items-gui",
-  TextUtils.simpleColorize(raw("Gui.editItems.title")),
-  3
+  "edit-items-gui",
+  TextUtils.simpleColorize(raw("Gui.editItems.title").replace("{region}", region.id)),
+  6
 ) {
-  val paginated = PaginationManager(this)
   override fun onOpen(event: InventoryOpenEvent?) {
-    super.onOpen(event)
+    load()
+
+    val back = ItemStack(Material.valueOf(raw("Gui.editItems.back.material")))
+    val backMeta = back.itemMeta
+    backMeta.displayName(cmp("Gui.editItems.back.name"))
+    back.itemMeta = backMeta
+
+    addItem(Icon(back).onClick {
+      RegionSettings(player, region).open()
+    }, 45)
+
+    loadItems()
+  }
+
+  private fun loadItems() {
+    val regionData = Region.load(region.id, AntiAbusers.instance)
+
+    regionData.regionData.blockedItems.forEach { item ->
+      paginationManager.addItem(Icon(item).onClick {
+        DeleteItem(player, regionData, region, item).open()
+      })
+    }
+
+    paginationManager.update()
   }
 }
