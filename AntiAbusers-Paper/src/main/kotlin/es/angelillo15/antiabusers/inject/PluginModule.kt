6c801eee9afc6dccfb810fba34b2ffe9e18c8182
Angelillo15 <contact@angelillo15.es>
2023-09-06 15:24:43 +0200

    Added new binds

--- AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/inject/PluginModule.kt
+++ AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/inject/PluginModule.kt
@@ -8,6 +8,7 @@ import es.angelillo15.antiabusers.AntiAbusersInstance
 import es.angelillo15.antiabusers.config.ConfigManager
 import es.angelillo15.antiabusers.config.Messages
 import es.angelillo15.antiabusers.manager.RegionManager
+import es.angelillo15.antiabusers.thread.AsyncExecutor
 import es.angelillo15.antiabusers.utils.PluginLogger
 import es.angelillo15.core.Logger
 import es.angelillo15.core.NookInstance
@@ -27,5 +28,6 @@ open class PluginModule : AbstractModule() {
     bind(CoreMessages::class.java).to(Messages::class.java).asEagerSingleton()
     bind(AudienceProvider::class.java).toInstance(BukkitAudiences.create(AntiAbusers.instance))
     bind(RegionManager::class.java).asEagerSingleton()
+    bind(AsyncExecutor::class.java).asEagerSingleton()
   }
 }
