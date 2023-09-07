a5923d69efaefe7bb200fc6ffae0d4f63dbce44d
Angelillo15 <contact@angelillo15.es>
2023-09-07 14:03:32 +0200

    Added config and prefix

--- AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/utils/TextUtils.kt
+++ AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/utils/TextUtils.kt
@@ -2,6 +2,7 @@ package es.angelillo15.antiabusers.utils
 
 import es.angelillo15.antiabusers.AntiAbusers
 import es.angelillo15.antiabusers.config.ConfigManager
+import es.angelillo15.antiabusers.config.Messages
 import es.angelillo15.core.libs.simpleyaml.configuration.file.YamlFile
 import es.angelillo15.core.utils.TextUtils
 import net.kyori.adventure.text.Component
@@ -12,13 +13,36 @@ fun raw(path: String): String {
 }
 
 fun tl(path: String): String {
-  return TextUtils.toMM(raw(path))
+  return TextUtils.toMM(
+    raw(
+      path.replace(
+        "{prefix}",
+        AntiAbusers.instance.pluginInjector.getInstance(Messages::class.java).prefix()
+      )
+    )
+  )
 }
 
 fun cmp(path: String): Component {
   return MiniMessage.miniMessage().deserialize(tl(path))
 }
 
-private fun getMessages() : YamlFile {
+fun string(path: String): String {
+  return getConfig().getString(path) ?: path
+}
+
+fun int(path: String): Int {
+  return getMessages().getInt(path)
+}
+
+fun bool(path: String): Boolean {
+  return getConfig().getBoolean(path)
+}
+
+private fun getMessages(): YamlFile {
   return AntiAbusers.instance.pluginInjector.getInstance(ConfigManager::class.java).messages!!.config
+}
+
+private fun getConfig(): YamlFile {
+  return AntiAbusers.instance.pluginInjector.getInstance(ConfigManager::class.java).config!!.config
 }
