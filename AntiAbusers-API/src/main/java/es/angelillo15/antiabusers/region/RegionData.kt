14e86b36260336526751448fdb8519befce91240
Angelillo15 <contact@angelillo15.es>
2023-09-07 14:02:51 +0200

    Added world

--- AntiAbusers-API/src/main/java/es/angelillo15/antiabusers/region/RegionData.kt
+++ AntiAbusers-API/src/main/java/es/angelillo15/antiabusers/region/RegionData.kt
@@ -3,4 +3,4 @@ package es.angelillo15.antiabusers.region
 import org.bukkit.inventory.ItemStack
 import java.io.Serializable
 
-class RegionData(val id: String, val blockedItems: ArrayList<ItemStack>) : Serializable
+class RegionData(val id: String, val world: String,val blockedItems: ArrayList<ItemStack>) : Serializable
