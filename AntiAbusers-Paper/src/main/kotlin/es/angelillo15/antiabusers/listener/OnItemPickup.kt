44d443557d61bb7227c3d74b409a834f98d1d508
Angelillo15 <contact@angelillo15.es>
2023-09-07 14:57:48 +0200

    Run delayed task in the next tick

--- AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/listener/OnItemPickup.kt
+++ AntiAbusers-Paper/src/main/kotlin/es/angelillo15/antiabusers/listener/OnItemPickup.kt
@@ -1,15 +1,20 @@
 package es.angelillo15.antiabusers.listener
 
 import com.google.inject.Inject
+import es.angelillo15.antiabusers.AntiAbusers
 import es.angelillo15.antiabusers.manager.AntiAbusersPlayers
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityPickupItemEvent
+import java.util.function.Consumer
 
 class OnItemPickup : Listener {
   @Inject
   private lateinit var antiAbusersPlayers: AntiAbusersPlayers
+  @Inject
+  private lateinit var plugin: AntiAbusers
 
   @EventHandler
   fun onItemPickup(event: EntityPickupItemEvent) {
@@ -18,6 +23,7 @@ class OnItemPickup : Listener {
     val player = event.entity as Player
     val antiAbuserPlayer = antiAbusersPlayers.getPlayer(player.name) ?: return
 
-    antiAbuserPlayer.check(true)
-  }
+    Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, Consumer {
+      antiAbuserPlayer.check(true)
+    }, 1)  }
 }
